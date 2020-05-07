53f18013e82ce6877c0a825c22b5dea90b7022d3
Zach Shewmaker <zach@azaia.net>
2020-05-06 17:30:14 -0700

    Added gas/supply labels and simple next turn behavior

---
+++ Assets/Scripts/GasComponent.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class GasComponent : MonoBehaviour
+{
+	public int Amount = 100;
+	public int MaxAmount = 100;
+}
---
+++ Assets/Scripts/NextTurnController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NextTurnController : MonoBehaviour
+{
+    public Button NextTurnButton;
+    public Text GasValueText;
+    public Text SupplyValueText;
+    public GasComponent GasComponent;
+    public SuppliesComponent SuppliesComponent;
+
+    void Start()
+    {
+        NextTurnButton.onClick.AddListener(OnNextTurn);
+    }
+
+    void OnNextTurn()
+    {
+        GasComponent.Amount--;
+        SuppliesComponent.Amount--;
+        GasValueText.text = GasComponent.Amount.ToString();
+        SupplyValueText.text = SuppliesComponent.Amount.ToString();
+    }
+}

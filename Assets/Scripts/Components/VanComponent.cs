9ebb2ec5a243fe2d8046520b928334169e089c1c
Zach Shewmaker <zach@azaia.net>
2020-07-19 11:38:18 -0700

    created resource debug panel

---
+++ Assets/Scripts/Components/VanComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class VanComponent : MonoBehaviour
+{
+    public VanContext Context;
+    public Resource[] Resources;
+
+    public delegate void ResourceChangeAction(Resource resource, VanContext context);
+    public static event ResourceChangeAction OnResourceChange;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public Resource GetResource(string key)
+    {
+        return Resources.FirstOrDefault(x => x.Key == key);
+    }
+
+    public void IncreaseResource(string key, float amount)
+    {
+        AdjustResource(key, resource => Math.Min(resource.MaxValue, resource.Value + amount));
+    }
+
+    public void DecreaseResource(string key, float amount)
+    {
+        AdjustResource(key, resource => Math.Max(0, resource.Value - amount));
+    }
+
+    public void AdjustResource(string key, Func<Resource, float> fn)
+    {
+        var resource = Resources.FirstOrDefault(x => x.Key == key);
+        if (resource == null) return;
+
+        resource.Value =fn(resource);
+        if (OnResourceChange == null) return;
+
+        OnResourceChange(resource, Context);
+    }
+}
---
+++ Assets/Scripts/VanContext.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+[Serializable]
+public struct VanContext
+{
+	public string Location;
+}

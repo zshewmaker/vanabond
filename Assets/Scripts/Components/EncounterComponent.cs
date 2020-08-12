76e141ebcd5ea64326178806dbebff436a6e76df
Zach Shewmaker <zach@azaia.net>
2020-08-12 12:56:27 -0700

    added encounters

---
+++ Assets/Scripts/Components/EncounterComponent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EncounterComponent : MonoBehaviour
+{
+    [SerializeField] private VanComponent Van;
+
+    void Start()
+    {
+        // This is what we need to make work next. Resources should be accessible through van context.
+        var encounter1 = new Encounter
+        {
+            EncounterTitle = "Bandits! OMGahz!",
+            OnEncounterOutcome = new Outcome(vanContext =>
+            {
+                // lose 5 supplies
+            }),
+            Actions =
+            {
+                { "Buy back supplies", new Outcome(vanContext => {
+                    // gain 5 supply
+                    // lose 10 money
+                }) },
+                { "Run!", new Outcome(vanContext => {
+                    // lose 2 gas
+                }) },
+            }
+        };
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Encounter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Encounter
+{
+    public string EncounterTitle { get; set; }
+    public Outcome OnEncounterOutcome { get; set; }
+    public IDictionary<string, Outcome> Actions { get; set; }
+}
+
+[Serializable]
+public class Outcome
+{
+    private readonly Action<VanContext> action;
+
+    public Outcome(Action<VanContext> action)
+    {
+        this.action = action;
+    }
+
+    public void Execute(VanContext vanContext)
+    {
+        action(vanContext);
+    }
+}
+
+//public class Stuff
+//{
+//    public void Thing()
+//    {
+//        var encounter = new Encounter
+//        {
+//            EncounterTitle = "the encounter",
+//            OnEncounterOutcome = new Outcome(vanContext => { /*do stuff */}),
+//            Actions =
+//            {
+//                { "", new Outcome(vanContext => { }) }
+//            }
+//        };
+//    }
+//}
